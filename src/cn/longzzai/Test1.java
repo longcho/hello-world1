b7406104122ad70f4a1476a7add932c4169c04c0
longcho <longchonk@gmail.com>
2017-08-23 15:56:02 +0800

    测试自定义description

--- src/cn/longzzai/Test1.java
+++ src/cn/longzzai/Test1.java
@@ -2,11 +2,17 @@ package cn.longzzai;
 
 
 public class Test1 {
+    /**
+     *
+     * @param args
+     *
+     *
+     */
     public static void main(String[] args) {
         System.out.println("hello-world");
         System.out.println("hello-world2");
         System.out.println("hello-world3");
-        System.out.println("hello-world5");
+        System.out.println("hello-world4");
     }
 }
 
