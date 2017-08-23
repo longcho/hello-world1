88a046e7a531b22beb94b63e0f33d99682f3d75f
longcho <longchonk@gmail.com>
2017-08-23 15:54:34 +0800

    测试自定义description

---
+++ src/cn/longzzai/Test2.java
@@ -0,0 +1,9 @@
+package cn.longzzai;
+/**
+ * 测试description
+ * @author longcho
+ * @create 2017-08-23-15:50
+ */
+public class Test2 {
+
+}
---
+++ src/cn/longzzai/Test3.java
@@ -0,0 +1,10 @@
+package cn.longzzai;
+
+/**
+ * 第二次测试description
+ * @author longcho
+ * @create 2017-08-23-15:51
+ */
+public class Test3 {
+
+}
