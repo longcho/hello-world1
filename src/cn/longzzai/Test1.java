1cc22863a3a53fc11a33886378ca3ee345105421
longcho <longchonk@gmail.com>
2017-08-23 15:02:21 +0800

    Initial commit

---
+++ src/cn/longzzai/Test1.java
@@ -0,0 +1,11 @@
+package cn.longzzai;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello-world");
+        System.out.println("hello-world2");
+        System.out.println("hello-world3");
+    }
+}
+
